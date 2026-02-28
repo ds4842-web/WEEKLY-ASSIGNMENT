039cf0d8c4e1b510117dc2971a4609de94fafe56
ds4842-web <ds4842@srmist.edu.in>
2026-02-28 14:28:33 +0530

    file lvl1_2 added to branch week-1/2

---
+++ lvl1_2.java
@@ -0,0 +1,10 @@
+public class lvl1_2 {
+    public static void main(String[] args){
+        int mathMarks = 94, physicsMarks = 95, chemistryMarks = 96, totalMarks, avgMarks;
+        totalMarks = mathMarks + physicsMarks + chemistryMarks;
+        avgMarks = totalMarks / 3;
+        System.out.println("Sam's average mark in PCM is " + avgMarks);
+    
+    }
+    
+}
