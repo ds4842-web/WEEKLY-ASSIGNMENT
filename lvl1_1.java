9e0adc9fbe3c88034d27c434a17379ffcaa25989
ds4842-web <ds4842@srmist.edu.in>
2026-02-28 14:00:18 +0530

    file lvl1_1 added to branch week-1/2

---
+++ lvl1_1.java
@@ -0,0 +1,10 @@
+class lvl1_1 {
+    public static void main(String[] args){
+        int age, birsthYear = 2000, currentYear = 2024;
+        age = currentYear - birsthYear;
+        System.out.println("Harry's age is " + age);
+
+
+    }
+    
+}
