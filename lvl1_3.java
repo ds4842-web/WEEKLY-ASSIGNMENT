cef2418b34164ec0f89d59171169386c62a961e6
ds4842-web <ds4842@srmist.edu.in>
2026-02-28 14:47:30 +0530

    new file lvl1_3 is added to branch week-1/2

---
+++ lvl1_3.java
@@ -0,0 +1,9 @@
+public class lvl1_3 {
+    public static void main(String[] args) {
+        double km = 10.8, conversionFactor = 1.6, convertedMiles;
+        convertedMiles = km / conversionFactor;
+        System.out.println("The distance " + km + "km in miles is " + convertedMiles);
+    }
+}
+    
+
