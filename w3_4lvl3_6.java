70197eb76530d1b33840d667939b35ae1f860b5b
ds4842-web <ds4842@srmist.edu.in>
2026-03-01 20:15:47 +0530

    Added code solution for level 3 question 6 as file w3_4lvl3_6.java in branch week-3/4

---
+++ w3_4lvl3_6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class w3_4lvl3_6 {
+    public static void main(String[] args){
+        try(Scanner sc = new Scanner(System.in)){
+            int number, count = 0;
+            System.out.print("Enter a number: ");
+            number = sc.nextInt();
+            while(number != 0){
+                number /= 10;
+                count++;
+            }
+            System.out.println("Number of digits is " + count);
+
+
+        }
+    }
+    
+}
